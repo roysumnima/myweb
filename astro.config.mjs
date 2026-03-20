56eafd07a07225807d5a1d8ea94a8bcb9f8e5f50
roy <roy.sumnima@gmail.com>
2026-03-20 15:10:43 +0000

    Move assets?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { BASE_URL } from './src/assets/data/webdetails';
+import { BASE_URL } from './public/assets/data/webdetails';
 
 // https://astro.build/config
 export default defineConfig({
