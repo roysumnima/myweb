e34a09060565cc703b80d1cc190136ade3e3aad0
Sumnima Roy <157907220+roysumnima@users.noreply.github.com>
2026-03-23 11:41:04 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import { BASE_URL } from './src/assets/data/webdetails';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://roysumnima.github.io',
+  base: `/${BASE_URL}`,
 });
